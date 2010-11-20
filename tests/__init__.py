a8272cd5e17ca58c0c181730a535dcb9181b5140
Francisco Souza <francisco@franciscosouza.net>
2010-11-20 18:23:57 -0200

    Added tests structure

---
+++ tests/__init__.py
@@ -0,0 +1,17 @@
+from mocker import MockerTestCase
+
+class BaseTestCase(MockerTestCase):
+
+    def setUp(self):
+        super(BaseTestCase, self).setUp()
+        self.setup()
+
+    def tearDown(self):
+        super(BaseTestCase, self).tearDown()
+        self.teardown()
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
