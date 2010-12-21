eac39222fe4ee8679b7062129fc4863f121c870d
Francisco Souza <francisco@franciscosouza.net>
2010-12-21 20:00:31 -0200

    Supporting POST on /books/new, saving the book on database and redirecting to /books, which lists all books

--- examples/books_collection/collection/views.py
+++ examples/books_collection/collection/views.py
@@ -1,8 +1,17 @@
 from collection import app
 from collection.forms import BookForm
-from flask import render_template
+from collection.documents import Book
+from flask import render_template, redirect, url_for
 
-@app.route('/books/new')
+@app.route('/books/new', methods=['GET', 'POST'])
 def new_book():
     form = BookForm()
+    if form.validate_on_submit():
+        form.save()
+        return redirect(url_for('list_books'))
     return render_template('/books/new.html', form=form)
+
+@app.route('/books')
+def list_books():
+    books = Book.query.all()
+    return render_template('/books/list.html', books=books)
