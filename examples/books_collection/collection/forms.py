bf853b3e215f1a9018007cb6efc5cc027c447a33
Francisco Souza <francisco@franciscosouza.net>
2010-12-21 20:25:28 -0200

    Document instance made acessible on form

--- examples/books_collection/collection/forms.py
+++ examples/books_collection/collection/forms.py
@@ -5,22 +5,22 @@ class BookForm(wtf.Form):
     document_class = Book
     title = wtf.TextField(validators=[wtf.Required()])
     year = wtf.IntegerField(validators=[wtf.Required()])
-    _instance = None
+    instance = None
 
     def __init__(self, document=None, *args, **kwargs):
         super(BookForm, self).__init__(*args, **kwargs)
         if document is not None:
-            self._instance = document
+            self.instance = document
             self._copy_data_to_form()
 
     def _copy_data_to_form(self):
-        self.title.data = self._instance.title
-        self.year.data = self._instance.year
+        self.title.data = self.instance.title
+        self.year.data = self.instance.year
 
     def save(self):
-        if self._instance is None:
-            self._instance = self.document_class()
-        self._instance.title = self.title.data
-        self._instance.year = self.year.data
-        self._instance.save()
-        return self._instance
+        if self.instance is None:
+            self.instance = self.document_class()
+        self.instance.title = self.title.data
+        self.instance.year = self.year.data
+        self.instance.save()
+        return self.instance
