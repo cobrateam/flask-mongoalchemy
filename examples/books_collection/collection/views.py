cda8acc53cdcf07c88bbbef02aa85131c46c9716
Francisco Souza <francisco@franciscosouza.net>
2010-12-21 20:25:53 -0200

    Added a view who updates the book on database (receiving data via POST)

--- examples/books_collection/collection/views.py
+++ examples/books_collection/collection/views.py
@@ -27,3 +27,14 @@ def edit_book(id):
     book = Book.get(id)
     form = BookForm(document=book)
     return render_template('/books/edit.html', form=form, book=book)
+
+@app.route('/books/edit/<id>', methods=['POST'])
+def update_book(id):
+    book = Book.get(id)
+    form = BookForm()
+    if form.validate_on_submit():
+        form.instance = book
+        form.save()
+        return redirect(url_for('list_books'))
+    form = BookForm(document=book)
+    return render_template('/books/edit.html', form=form, book=book)
