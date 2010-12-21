6ffba0487f2779fac72649a1c4eaa9ac97e70b3f
Francisco Souza <francisco@franciscosouza.net>
2010-12-21 19:48:56 -0200

    typo

--- examples/books_collection/collection/__init__.py
+++ examples/books_collection/collection/__init__.py
@@ -3,10 +3,8 @@ from flaskext.mongoalchemy import MongoAlchemy
 
 app = Flask(__name__)
 app.config['MONGOALCHEMY_DATABASE'] = 'books_collection'
-app.config['SESSION_KEY'] = 'very secret, do you believe?'
+app.config['SECRET_KEY'] = 'very secret, do you believe?'
 app.config['DEBUG'] = True
 db = MongoAlchemy(app)
 
-from forms import BookForm
-from documents import Book
 from views import *
