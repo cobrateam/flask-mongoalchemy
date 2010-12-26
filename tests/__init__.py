29e89886c0bbc7ff6554b8b5b376019fa18f6a37
Francisco Souza <francisco@franciscosouza.net>
2010-12-26 02:03:14 -0200

    Added BaseAppTestCase class

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,7 @@
 from mocker import MockerTestCase
+from flask import Flask
+from flaskext import mongoalchemy
+from tests.helpers import _make_todo_document
 
 class BaseTestCase(MockerTestCase):
 
@@ -15,3 +18,16 @@ class BaseTestCase(MockerTestCase):
 
     def teardown(self):
         pass
+
+class BaseAppTestCase(BaseTestCase):
+
+    def setup(self):
+        self.app = Flask(__name__)
+        self.app.config['MONGOALCHEMY_DATABASE'] = 'testing'
+        self.app.config['TESTING'] = True
+        self.db = mongoalchemy.MongoAlchemy(self.app)
+        self.Todo = _make_todo_document(self.db)
+
+    def teardown(self):
+        for todo in self.Todo.query.all():
+            todo.remove()
