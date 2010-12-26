70d952f62fd616c0290fac6b9d67fe915d015afb
Francisco Souza <francisco@franciscosouza.net>
2010-12-26 02:51:22 -0200

    Added a context processor on example application

--- examples/books_collection/collection/__init__.py
+++ examples/books_collection/collection/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flaskext.mongoalchemy import MongoAlchemy
+import string
 
 app = Flask(__name__)
 app.config['MONGOALCHEMY_DATABASE'] = 'books_collection'
 app.config['SECRET_KEY'] = 'very secret, do you believe?'
 app.config['DEBUG'] = True
 db = MongoAlchemy(app)
+
+@app.context_processor
+def put_letters_on_request():
+    return { 'letters' : string.ascii_uppercase }
 
 from views import *
