6ce5146ffb0f7a9878a7c28de44a4e0172347df9
Francisco Souza <francisco@franciscosouza.net>
2010-12-26 02:42:16 -0200

    Added a customized query class

--- examples/books_collection/collection/documents.py
+++ examples/books_collection/collection/documents.py
@@ -1,5 +1,15 @@
 from collection import db
+from flaskext.mongoalchemy import BaseQuery
+import re
+
+class BookQuery(BaseQuery):
+
+    def starting_with(self, letter):
+        regex = r'^' + letter
+        return self.filter({'title' : re.compile(regex, re.IGNORECASE)})
 
 class Book(db.Document):
+    query_class = BookQuery
+
     title = db.StringField()
     year = db.IntField()
