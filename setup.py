1f32b879d7647c67118d0b3cbbf95aa3cd0f5fa3
Francisco Souza <francisco@franciscosouza.net>
2011-08-24 21:02:00 -0300

    Bump to 0.4.3

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ with open('README.rst') as readme_file:
 
 setup(
     name='Flask-MongoAlchemy',
-    version='0.4.2',
+    version='0.4.3',
     url='http://github.com/cobrateam/flask-mongoalchemy',
     license='BSD',
     author='Francisco Souza',
