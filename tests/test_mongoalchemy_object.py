a6da9d2eb415affae0a9cb5e26121ca2cd3ca97a
Francisco Souza <f@souza.cc>
2011-09-10 14:20:17 -0300

    Refactoring tests

--- tests/test_mongoalchemy_object.py
+++ tests/test_mongoalchemy_object.py
@@ -1,7 +1,12 @@
-from tests import BaseTestCase
-from nose.tools import assert_true, assert_equals, raises
+# -*- coding: utf-8 -*-
 from flask import Flask
-from flaskext.mongoalchemy import ImproperlyConfiguredError
+from mongoalchemy import fields
+from mongoalchemy.session import Session
+from nose.tools import assert_true, assert_equals, raises
+
+from flaskext.mongoalchemy import BaseQuery, ImproperlyConfiguredError, MongoAlchemy
+from tests import BaseTestCase
+
 
 class MongoAlchemyObjectTestCase(BaseTestCase):
     "MongoAlchemy itself object tests"
@@ -12,25 +17,20 @@ class MongoAlchemyObjectTestCase(BaseTestCase):
         self.app.config['TESTING'] = True
 
     def should_provide_a_Document_class_to_be_extended_inside_the_MongoAlchemy_object(self):
-        from flaskext.mongoalchemy import MongoAlchemy
         db = MongoAlchemy()
         assert db.Document is not None
 
     def should_provide_a_query_object_for_queries_on_a_document(self):
-        from flaskext.mongoalchemy import MongoAlchemy, BaseQuery
         db = MongoAlchemy(self.app)
         class Todo(db.Document):
             description = db.StringField()
         assert_true(isinstance(Todo.query, BaseQuery))
 
     def should_provide_a_session_object_on_mongoalchemy_instance(self):
-        from flaskext.mongoalchemy import MongoAlchemy
-        from mongoalchemy.session import Session
         db = MongoAlchemy(self.app)
         assert_true(isinstance(db.session, Session))
 
     def should_be_possible_to_create_a_customized_query_class(self):
-        from flaskext.mongoalchemy import MongoAlchemy, BaseQuery
         db = MongoAlchemy(self.app)
         class Query(BaseQuery):
             pass
@@ -42,7 +42,6 @@ class MongoAlchemyObjectTestCase(BaseTestCase):
         assert_true(isinstance(Todo.query, Query))
 
     def should_set_None_to_query_attribute_on_Document_when_queryclass_does_not_extends_BaseQuery(self):
-        from flaskext.mongoalchemy import MongoAlchemy
         db = MongoAlchemy()
         class Query(object):
             pass
@@ -54,21 +53,16 @@ class MongoAlchemyObjectTestCase(BaseTestCase):
         assert Todo.query is None
 
     def should_include_all_mongo_alchemy_fields_objects(self):
-        from mongoalchemy import fields
-        from flaskext.mongoalchemy import MongoAlchemy
-
         db = MongoAlchemy()
         for key in dir(fields):
             assert_true(hasattr(db, key))
         assert_true(hasattr(db, 'DocumentField'))
 
     def should_be_able_to_instantiate_passing_the_app(self):
-        from flaskext.mongoalchemy import MongoAlchemy
         db = MongoAlchemy(self.app)
         assert db.session is not None
 
     def should_be_able_to_instantiate_without_passing_the_app_and_set_it_later(self):
-        from flaskext.mongoalchemy import MongoAlchemy
         db = MongoAlchemy()
         assert db.session is None
         db.init_app(self.app)
@@ -76,7 +70,6 @@ class MongoAlchemyObjectTestCase(BaseTestCase):
 
     def should_contain_a_not_none_query(self):
         "Document.query should never be None"
-        from flaskext.mongoalchemy import MongoAlchemy
         db = MongoAlchemy()
         db.init_app(self.app)
 
@@ -88,12 +81,10 @@ class MongoAlchemyObjectTestCase(BaseTestCase):
 
     @raises(ImproperlyConfiguredError)
     def test_shout_not_be_able_to_work_without_providing_a_database_name(self):
-        from flaskext.mongoalchemy import MongoAlchemy
         app = Flask('new_test')
         db = MongoAlchemy(app)
 
     def should_be_able_to_work_without_providing_server_port_user_and_password_for_database_connection(self):
-        from flaskext.mongoalchemy import MongoAlchemy
         app = Flask('newest_test')
         app.config['MONGOALCHEMY_DATABASE'] = 'my_database'
         db = MongoAlchemy(app)
@@ -101,6 +92,9 @@ class MongoAlchemyObjectTestCase(BaseTestCase):
         assert_equals(app.config['MONGOALCHEMY_PORT'], '27017')
         assert_equals(app.config['MONGOALCHEMY_USER'], None)
         assert_equals(app.config['MONGOALCHEMY_PASSWORD'], None)
+
+    def should_be_able_to_create_two_decoupled_mongoalchemy_instances(self):
+        app = Flask('new_test')
 
     def teardown(self):
         del(self.app)
