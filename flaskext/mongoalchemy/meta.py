e3b0887588aeeb5f72c5d80efb514e09bcf1534d
Francisco Souza <f@souza.cc>
2011-09-10 15:03:23 -0300

    Fixed make_document_class function

--- flaskext/mongoalchemy/meta.py
+++ flaskext/mongoalchemy/meta.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
-from flaskext.mongoalchemy import Document
-
 from mongoalchemy import document
 
 
-def make_document_class(session):
-    class_dict = Document.__dict__.copy()
+def make_document_class(session, document_class):
+    class_dict = document_class.__dict__.copy()
     class_dict.update({ '_session' : session })
 
     return type("Document", (document.Document,), class_dict)
--- tests/test_meta.py
+++ tests/test_meta.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 
 from mongoalchemy import document
 
+from flaskext.mongoalchemy import Document
 from flaskext.mongoalchemy.meta import make_document_class
 from tests import BaseAppTestCase
 
@@ -11,5 +12,5 @@ class MetaTestCase(BaseAppTestCase):
     "meta.py test case"
 
     def should_be_able_to_create_a_new_document(self):
-        Document = make_document_class(self.db)
-        assert issubclass(Document, document.Document)
+        MyDocument = make_document_class(self.db, Document)
+        assert issubclass(MyDocument, document.Document)
