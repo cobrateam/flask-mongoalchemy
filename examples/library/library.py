5db40467c03db191115a12402f0145e738cd147d
Francisco Souza <f@souza.cc>
2014-04-22 21:05:38 -0300

    examples/library: fix flake8 issues

--- examples/library/library.py
+++ examples/library/library.py
@@ -11,13 +11,16 @@ app.config['DEBUG'] = True
 app.config['MONGOALCHEMY_DATABASE'] = 'library'
 db = MongoAlchemy(app)
 
+
 class Author(db.Document):
     name = db.StringField()
+
 
 class Book(db.Document):
     title = db.StringField()
     author = db.DocumentField(Author)
     year = db.IntField()
+
 
 @app.route('/author/new')
 def new_author():
@@ -28,6 +31,7 @@ def new_author():
     author = Author(name=request.args.get('name', ''))
     author.save()
     return 'Saved :)'
+
 
 @app.route('/authors/')
 def list_authors():
